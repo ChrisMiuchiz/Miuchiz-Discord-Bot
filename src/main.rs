2107f002d2c28f7a2473449ebba84a741d553e5b
ChrisMiuchiz <chris000005@gmail.com>
2022-08-23 20:07:32 -0400

    Don't allow the bot to start multiple instances of itself upon connection issues

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,39 @@ mod miuchiz;
 
 struct Handler {
     user_file: PathBuf,
+    bot_dispatched: RwLock<bool>,
 }
 
 impl Handler {
     pub fn new(user_file: PathBuf) -> Self {
-        Self { user_file }
+        Self {
+            user_file,
+            bot_dispatched: RwLock::new(false),
+        }
+    }
+
+    async fn enter_bot(&self) -> bool {
+        let mut bot_dispatched = self.bot_dispatched.write().await;
+
+        if *bot_dispatched {
+            return false;
+        }
+
+        *bot_dispatched = true;
+
+        true
     }
 }
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, ready: Ready) {
+        // Allow only one instance of the bot, the current function could be
+        // called again if there is a connection issue.
+        if !self.enter_bot().await {
+            return;
+        }
+
         let mut bot = miuchiz::MiuchizBot::new(self.user_file.clone(), ctx.clone()).await;
         println!("{} is connected!", ready.user.name);
         tokio::spawn(async move {
